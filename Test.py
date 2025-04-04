6400d9fbc4f910c009a1e336751e1911ab15c687
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-05 01:25:25 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,5 +5,5 @@ class TestExamplePlugin(unittest.TestCase):
     def test_greet(self):
         self.assertEqual(greet("Abhi"), "Hello, Abhi! This is a plugin.")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
