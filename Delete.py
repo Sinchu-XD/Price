100f0630e9dd67a3a04f37e274bbdaccf7011eaa
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-11 17:54:42 +0530

    Update Delete.py

--- Delete.py
+++ Delete.py
@@ -6,9 +6,6 @@ API_ID = 6067591
 API_HASH = "94e17044c2393f43fda31d3afe77b26b"
 BOT_TOKEN = "8101654222:AAGyL2ScVLhYTrQJNWU8urw93DqcLRCe2iA"
 
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
 
 bot = Client("TesthBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
